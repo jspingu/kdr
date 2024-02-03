94824417f03d905d700c4dfdc25e88ed547673d3
Jay <jsim3368@gmail.com>
2024-02-03 20:04:58 +0800

    Moved rectangle mesh generator to MeshBuilder class

--- src/world/Mesh.cs
+++ src/world/Mesh.cs
@@ -12,16 +12,4 @@ public class Mesh
         TextureVertices = textureVertices;
         Faces = faces;
     }
-
-    public static Mesh CreateRectangleMesh(int width, int height)
-    {
-        float halfWidth = width / 2f;
-        float halfHeight = height / 2f;
-
-        return new Mesh(
-            new Vector3[] {new(-halfWidth, halfHeight, 0), new(halfWidth, halfHeight, 0), new(-halfWidth, -halfHeight, 0), new(halfWidth, -halfHeight, 0)},
-            new Vector2[] {new(0, 0), new(1, 0), new(0, 1), new(1, 1)},
-            new IndexedFace[] {new(0, 1, 2, 0, 1, 2), new(3, 2, 1, 3, 2, 1)}
-        );
-    }
 }
